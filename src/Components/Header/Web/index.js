284cffa4252225bd189a3ef85d726b553c2353f2
UsmanAsif193 <82535372+UsmanAsif193@users.noreply.github.com>
2021-07-26 00:25:57 +0500

    Header Styling for mobile / divided into parts added

---
+++ src/Components/Header/Web/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Web() {
+    return (
+        <div>
+            This is Web
+        </div>
+    )
+}
+
+export default Web
--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Header.css'
+import Mobile from './Mobile'
+import Web from './Web'
 
 function Header() {
+    const [isOpen, setIsOpen] = useState(false)
     return (
         <div className='header'>
-            This is header
+            <div className="logo">Usman Asif</div>
+            <div className="menu">
+                <div className="web-men"><Web /></div>
+                <div className="mobile-menu">
+                    <div onClick={() => { setIsOpen(!isOpen) }}>
+                        <i class="fi-rr-apps menu-icon"></i>
+                    </div>
+                    {isOpen && <Mobile isOpen={isOpen} setIsOpen={setIsOpen} />}
+                </div>
+            </div>
         </div>
     )
 }
