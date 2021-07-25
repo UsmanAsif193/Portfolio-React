46723de4b9013d6dcb6a1cfd2a7fe30871693911
UsmanAsif193 <82535372+UsmanAsif193@users.noreply.github.com>
2021-07-25 23:47:00 +0500

    Basic Home styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Home from './Components/Home/index '
+import Home from './Components/Home/index'
 
 function App() {
   return (
--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import './Home.css'
+import Header from '../Header/index'
+import Body from '../Body/index'
+import Footer from '../Footer/index'
 
 function Home() {
     return (
         <div className='home'>
-            This is Home
+            <Header />
+            <Body />
+            <Footer />
         </div>
     )
 }
