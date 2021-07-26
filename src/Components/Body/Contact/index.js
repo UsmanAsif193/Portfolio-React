73e6df85fde4bbede8fdfa207df237f06c0c8f57
UsmanAsif193 <82535372+UsmanAsif193@users.noreply.github.com>
2021-07-27 01:01:11 +0500

    Contact section completed with section header styling

--- src/Components/Body/Contact/index.js
+++ src/Components/Body/Contact/index.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import Seperator from '../../Common/Seperator'
 import './Contact.css'
+import SocailContact from '../../Common/Social-Contact/index'
 
 function Contact() {
     return (
         <div className='contact'>
-            This is Contact
+            <Seperator />
+            <label className="section-title">Contact</label>
+            <div className="contact-container">
+                <div className="contact-left">
+                    <p>Want to get in touch? Contact me on any of the platform</p>
+                    <SocailContact />
+                </div>
+                <div className="download">
+                    <a download href="#"><i class="fi-rr-cloud-download download-icon"></i>Download Resume</a>
+                </div>
+            </div>
         </div>
     )
 }
