fd57c91f598239986df1ac998a8845642094cc97
UsmanAsif193 <82535372+UsmanAsif193@users.noreply.github.com>
2021-07-26 16:47:22 +0500

    About top part completed

--- src/Components/Body/About/index.js
+++ src/Components/Body/About/index.js
@@ -1,10 +1,26 @@
-import React from 'react'
-import './About.css'
+import React from 'react';
+import './About.css';
+import aboutCoding from '../../../assets/coding2.jpg';
 
 function About() {
     return (
         <div className='about'>
-            This is About
+            <div className="about-top">
+                <div className="about-info">
+                    Hello There 👋, I am
+                    <br /><span className='info-name'>Usman Asif</span>.
+                    <br />I am an expert of ReactJS.
+                </div>
+                <div className="about-photo">
+                    <img
+                        src={aboutCoding}
+                        alt="About-Coding"
+                        className='about-picture' />
+                </div>
+            </div>
+            <div className="about-bottom">
+                This is Contacts
+            </div>
         </div>
     )
 }
