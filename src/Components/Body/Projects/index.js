811bf7c0acea8feb4c2a34bbfe4cf3d2dfe81e36
UsmanAsif193 <82535372+UsmanAsif193@users.noreply.github.com>
2021-07-26 22:25:33 +0500

    Seperator file created and used in projects

--- src/Components/Body/Projects/index.js
+++ src/Components/Body/Projects/index.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { ProjectData } from '../../Data/Projects'
 import './Projects.css'
 import ProjectCard from './ProjectCard'
+import Seperator from '../../Common/Seperator'
 
 function Projects() {
     const data = ProjectData
     return (
         <div className='projects'>
+            <Seperator />
             <label className='section-title'>Projects</label>
             {data.map((project) => { return <ProjectCard project={project} /> })}
         </div>
