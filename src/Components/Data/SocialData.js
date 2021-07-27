968b994008fdb6b4194fc2b5fc899daad4161fa8
UsmanAsif193 <82535372+UsmanAsif193@users.noreply.github.com>
2021-07-27 15:49:35 +0500

    Link issue resolved and will open in new tab

--- src/Components/Data/SocialData.js
+++ src/Components/Data/SocialData.js
@@ -1,27 +1,27 @@
 export const SocialData = [
     {
         platform: 'Instagram',
-        link: 'www.instagram.com/UsmanAsif193',
+        link: 'https://www.instagram.com/UsmanAsif193',
         icon: require('../../assets/icon/instagram.png').default
     },
     {
         platform: 'Facebook',
-        link: 'www.facebook.com/UsmanAsif193',
+        link: 'https://www.facebook.com/UsmanAsif193',
         icon: require('../../assets/icon/facebook.png').default
     },
     {
         platform: 'LinkedIn',
-        link: 'www.Linkedin.com/in/UsmanAsif193',
+        link: 'https://www.Linkedin.com/in/UsmanAsif193',
         icon: require('../../assets/icon/linkedin.png').default
     },
     {
         platform: 'Twitter',
-        link: 'www.twitter.com/UsmanAsif193',
+        link: 'https://www.twitter.com/UsmanAsif193',
         icon: require('../../assets/icon/twitter.png').default
     },
     {
         platform: 'Github',
-        link: 'www.github.com/UsmanAsif193',
+        link: 'https://www.github.com/UsmanAsif193',
         icon: require('../../assets/icon/github.png').default
     },
 ]
