fe392d0646f2b0c3dd2a77b534458260d8c86f0d
Usman Asif <usmanasif193@gmail.com>
2021-08-12 00:01:47 +0500

    Header Completed of dark mode

--- src/Components/Header/Mobile/index.js
+++ src/Components/Header/Mobile/index.js
@@ -17,7 +17,7 @@ function Mobile({ isOpen, setIsOpen }) {
             <div className="close-icon" onClick={() => { setIsOpen(!isOpen) }}>
                 <i className="fi-rr-cross-circle"></i>
             </div>
-            <div className='mobile-options'>
+            <div className={darkMode ? "mobile-options dark-mob-options" : 'mobile-options'}>
                 <div className='mobile-option'>
                     <a href="#Projects">
                         <i className="fi-rr-edit-alt option-icon"></i>Projects
--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,13 +1,15 @@
 import React, { useState } from 'react'
+import { useDarkMode } from '../../Context/DarkMode'
 import './Header.css'
 import Mobile from './Mobile'
 import Web from './Web'
 
 function Header() {
     const [isOpen, setIsOpen] = useState(false)
+    const darkMode = useDarkMode()
     return (
         <div className='header'>
-            <div className="logo">Usman Asif</div>
+            <div className={darkMode ? "logo dark-logo" : "logo"}>Usman Asif</div>
             <div className="menu">
                 <div className="web-menu"><Web /></div>
                 <div className="mobile-menu">
