8ea0accade9629c39362e03951e0a82e75facfc9
Usman Asif <usmanasif193@gmail.com>
2021-08-12 15:25:12 +0500

    Dynamic App Loader added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import React, { useState } from 'react'
+import React, { Suspense } from 'react'
 import './App.css';
 import AppLoader from './Components/Common/App-Loader';
-import Home from './Components/Home/index'
 import { useDarkMode } from './Context/DarkMode';
 
+const Home = React.lazy(() => import('./Components/Home/index'))
+
 function App() {
-  const [isLoader, setIsLoader] = useState(false)
-
   const darkMode = useDarkMode()
-
-  setTimeout(() => {
-    setIsLoader(true)
-  }, 2000)
-
   return (
     <div className={`app-container ${darkMode ? "dark" : "light"}`}>
-      {isLoader ? (
+      <Suspense fallback={<AppLoader />}>
         <Home />
-      ) : (
-        <AppLoader />
-      )}
+      </Suspense>
     </div>
   );
 }
