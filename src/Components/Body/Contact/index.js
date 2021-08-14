235fe2be22cb08df8ab76289c76cb0e7326c86a3
Usman Asif <usmanasif193@gmail.com>
2021-08-14 23:03:07 +0500

    Download Button color changed in dark mode

--- src/Components/Body/Contact/index.js
+++ src/Components/Body/Contact/index.js
@@ -3,8 +3,10 @@ import Seperator from '../../Common/Seperator'
 import './Contact.css'
 import SocailContact from '../../Common/Social-Contact/index'
 import Resume from '../../../assets/Resume-Usman.pdf'
+import { useDarkMode } from '../../../Context/DarkMode'
 
 function Contact() {
+    const darkMode = useDarkMode()
     return (
         <div className='contact'>
             <Seperator />
@@ -14,7 +16,7 @@ function Contact() {
                     <p>Want to get in touch? Contact me on any of the platform</p>
                     <SocailContact />
                 </div>
-                <div className="download">
+                <div className={darkMode ? "download dark-download" : "download"}>
                     <a download href={Resume}><i className="fi-rr-cloud-download download-icon"></i>Download Resume</a>
                 </div>
             </div>
