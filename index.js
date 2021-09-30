72998bf8e1ce685f2bb187c9b9db50b58e3b5d42
Usman Asif <usmanasif193@gmail.com>
2021-09-30 19:06:27 +0500

    Folder updated for heroku deployment

--- index.js
+++ index.js
@@ -12,6 +12,14 @@ app.use(express.json());
 
 const port = process.env.PORT || 8000
 
+if(process.env.NODE_ENV == "production"){
+    app.use(express.static("client/build"));
+    const path =require('path');
+    app.get('*', (req,res) =>{
+        res.sendFile(path.resolve(__dirname, 'client','build', 'index.html'));
+    })
+}
+
 app.listen(port, ()=>{
     console.log(`server listening to port : ${port}`)
 })
