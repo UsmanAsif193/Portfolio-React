a84d6415bf007cd91da99b5fabcdb51006aa96c4
Usman Asif <usmanasif193@gmail.com>
2021-10-13 17:46:15 +0500

    See all button and new skills added in skills section

--- src/Components/Body/Skills/index.js
+++ src/Components/Body/Skills/index.js
@@ -1,18 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Skills.css'
 import Seperator from '../../Common/Seperator'
 import SkillsData from '../../Data/Skills.json'
 import SkillCard from './SkillCard'
-
+import GradientButton from '../../Common/Gradient-Button'
 
 function Skills() {
-    const data = SkillsData
+    const [loadMore, setloadMore] = useState(3)
+    const [buttondisable, setbuttondisable] = useState(false)
+    const data = SkillsData;
+
+    const loadMoreProjects = () => {
+        setloadMore(data.length)
+        setbuttondisable(true)
+    }
+
     return (
         <div className='skills'>
             <Seperator />
             <label className="section-title">Skills</label>
             <div className="skills-container">
-                {data.map((item) => {
+                {data.slice(0, loadMore).map((item) => {
                     return (
                         <div className="skills-section" key={item.type}>
                             <label className="skills-section-title">{item.type}</label>
@@ -26,6 +34,11 @@ function Skills() {
                         </div>
                     )
                 })}
+                <GradientButton
+                buttonvalue={"See All"}
+                handleClick={loadMoreProjects}
+                buttondisable={buttondisable}
+            />
             </div>
         </div>
     )
