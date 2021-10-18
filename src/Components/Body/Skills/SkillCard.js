0cfdb9be5630557d8a920f879b8533b30c8c5344
Usman Asif <usmanasif193@gmail.com>
2021-10-18 14:45:06 +0500

    Netlify and surge added as skil

--- src/Components/Body/Skills/SkillCard.js
+++ src/Components/Body/Skills/SkillCard.js
@@ -7,9 +7,9 @@ function SkillCard({ skill }) {
     return (
         <div className="skill-card">
             {darkMode ? (
-                <div className="skill-icon"><i className={skill.darkiconClass}></i></div>
+                <div className="skill-icon"><i className={skill.darkiconClass} data-icon={skill.dataIcon}></i></div>
             ) : (
-                <div className="skill-icon"><i className={skill.iconClass}></i></div>
+                <div className="skill-icon"><i className={skill.iconClass} data-icon={skill.dataIconDark}></i></div>
             )}
             <label className="skill-name">{skill.name}</label>
         </div>
