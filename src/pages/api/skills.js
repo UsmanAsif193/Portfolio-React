6a0356069615fb2944b0639b91090cdab1626113
Usman Asif <82535372+UsmanAsif193@users.noreply.github.com>
2021-12-29 21:55:21 +0500

    Data coming from backend success

---
+++ src/pages/api/skills.js
@@ -0,0 +1,13 @@
+const { connectToDatabase } = require("../../lib/mongodb");
+
+async function handler(req, res) {
+  try {
+    let { db } = await connectToDatabase();
+    let posts = await db.collection("Skills").find({}).toArray();
+    return res.status(200).json(posts).end();
+  } catch (error) {
+    return res.status(500).end();
+  }
+}
+
+export default handler;
